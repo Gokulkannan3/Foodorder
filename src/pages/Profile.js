27648fa979eef9f9b0d72f7b31d2a0a4025d29a2
GOKUL-KANNAN-P <gokulkannanp.cse2021@citchennai.net>
2024-05-14 14:39:06 +0530

    profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../components/navi';
 import axios from 'axios';
-import Skeleton from '@mui/material/Skeleton';
-import Stack from '@mui/material/Stack';
 import { Link } from 'react-router-dom';
 import insta from '../images/instagram.png';
 import whatsapp from '../images/whatsapp.png';
@@ -37,59 +35,45 @@ export default function Profile() {
     }
   }, []);
 
-  if (!userData) {
-    return (
-      <Stack spacing={1}>
-        <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
-        <Skeleton variant="circular" width={40} height={40} />
-        <Skeleton variant="rectangular" width={210} height={60} />
-        <Skeleton variant="rounded" width={210} height={60} />
-      </Stack>
-    );
-  }
-
   return (
-    <div className='out'>
-      <div className='fixed top-0 w-full'>
-        <Navbar />
-      </div> 
-      <div className='h-full'>
-        <div className='flex justify-center text-3xl font-bold mt-44'>
+    <div className='flex flex-col h-screen'>
+      <Navbar />
+      <div className='flex-grow'>
+        <div className='flex justify-center text-3xl font-bold mt-16'>
           <p>User Details</p>
         </div>
-        <div className='flex justify-center mt-8'>
-          {userData && (
-            <div className='w-full md:w-3/ lg:w-1/2 xl:w-1/3 bg-gray-300 rounded-lg p-8'>
-              <p className='mb-2 text-xl'>
-                Name: {userData.firstname} {userData.lastname}
-              </p>
-              <p className='mb-2 text-xl'>Age: {userData.age}</p>
-              <p className='mb-2 text-xl'>Email: {userData.email}</p>
-              <p className='mb-2 text-xl'>Contact: {userData.contact}</p>
-              <p className='mb-2 text-xl'>Address: {userData.address}</p>
-            </div>
-          )}
+        <div className='flex justify-center'>
+            {userData && (
+                <div className='flex justify-center bg-red-400 w-96 rounded-lg h-auto items-center p-4 text-2xl mt-16'>
+                    <div>
+                        <p className='mb-2'>Name: {userData.firstname} {userData.lastname}</p>
+                        <p className='mb-2'>Date of Birth: {userData.dob}</p>
+                        <p className='mb-2'>Age: {userData.age}</p>
+                        <p className='mb-2'>Email: {userData.email}</p>
+                        <p className='mb-2'>Contact: {userData.contact}</p>
+                        <p className='mb-2'>Address: {userData.address}</p>
+                    </div>
+                </div>    
+            )}
         </div>
       </div>
-      <div>
-        <footer className="footer footer-center p-10 bg-black text-red-500 rounded">
-          <nav className="fnav grid grid-flow-col gap-4">
-            <Link to={'/about'} className="link link-hover hover:text-white text-xl">About</Link>
-            <Link to={'/contact'} className="link link-hover hover:text-white text-xl">Contact</Link>
-            <Link to={'/team'} className="link link-hover hover:text-white text-xl">Team</Link>
-          </nav> 
-          <nav>
-            <div className="grid grid-flow-col gap-4">
-              <a href="https://www.instagram.com/innum_vai/"><img className='cursor-pointer' src={insta} alt='insta'/></a>
-              <a href='https://wa.me/9941269128'><img className='cursor-pointer' src={whatsapp} alt='insta'/></a>
-              <a href='mailto:innumvai@gmail.com'><img className='cursor-pointer' src={mail} alt='insta'/></a>
-            </div>
-          </nav> 
-          <aside>
-            <p>Copyright © 2024 - All right reserved by Innumvai</p>
-          </aside>
-        </footer>
-      </div>
+      <footer className="footer footer-center p-10 bg-black text-red-500 rounded">
+        <nav className="fnav grid grid-flow-col gap-4">
+          <Link to={'/about'} className="link link-hover hover:text-white text-xl">About</Link>
+          <Link to={'/contact'} className="link link-hover hover:text-white text-xl">Contact</Link>
+          <Link to={'/team'} className="link link-hover hover:text-white text-xl">Team</Link>
+        </nav> 
+        <nav>
+          <div className="grid grid-flow-col gap-4">
+            <a href="https://www.instagram.com/innum_vai/"><img className='cursor-pointer' src={insta} alt='insta'/></a>
+            <a href='https://wa.me/9941269128'><img className='cursor-pointer' src={whatsapp} alt='insta'/></a>
+            <a href='mailto:innumvai@gmail.com'><img className='cursor-pointer' src={mail} alt='insta'/></a>
+          </div>
+        </nav> 
+        <aside>
+          <p>Copyright © 2024 - All right reserved by Innumvai</p>
+        </aside>
+      </footer>
     </div>
   );
 }
