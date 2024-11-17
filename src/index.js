35555bc6a6783cfb3d137b9916e470ec6ac4b305
GOKUL-KANNAN-P <gokulkannanp.cse2021@citchennai.net>
2024-11-17 19:37:58 +0530

    clarity

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-q3jyk0vbcaverpkl.us.auth0.com"
+      clientId="c5vetiT5IyY0qCATttho3hMY9XkhZPYx"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
